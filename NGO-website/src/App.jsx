fdb6d0d0515d6b7ffc023b0c842c907ca133c368
Niraj00425 <00nirajthakur@gmail.com>
2025-09-17 20:35:30 +0530

    Updated project files/folders

--- NGO-website/src/App.jsx
+++ NGO-website/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import VolunteerForm from './components/VolunteerForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' //toggle to work
 
 function App() {
   return (
